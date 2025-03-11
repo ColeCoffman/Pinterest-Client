1bc99ccb87565434e2f61a1520a6704bf8845d15
Cole Coffman <cole.coffman@gmail.com>
2025-03-11 14:25:50 -0400

    React Router / Page Init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-createRoot(document.getElementById('root')).render(
+import Auth from "./routes/auth/auth.jsx";
+import Create from "./routes/create/create.jsx";
+import Home from "./routes/home/home.jsx";
+import MainLayout from "./routes/layouts/mainLayout.jsx";
+import Posts from "./routes/posts/posts.jsx";
+import Profile from "./routes/profile/profile.jsx";
+import Search from "./routes/search/search.jsx";
+
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/pin/:id" element={<Posts />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/search" element={<Search />} />
+        </Route>
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
