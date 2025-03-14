ec174deac539ac29eb3fd541bc8f20f41f3459a9
Cole Coffman <cole.coffman@gmail.com>
2025-03-14 14:29:15 -0400

    Create page layout

--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -1,33 +1,35 @@
+import { Link } from "react-router";
 import Image from "../image/image";
+
 import "./leftBar.css";
 
 const LeftBar = () => {
   return (
     <div className="leftBar">
       <div className="menuIcons">
-        <a href="/" className="menuIcon">
+        <Link to="/" className="menuIcon">
           <Image
             path="pinterest/general/logo.png"
             alt="Logo"
             className="logo"
           />
-        </a>
-        <a href="/" className="menuIcon">
+        </Link>
+        <Link to="/" className="menuIcon">
           <Image path="pinterest/general/home.svg" alt="Home" />
-        </a>
-        <a href="/" className="menuIcon">
+        </Link>
+        <Link to="/create" className="menuIcon">
           <Image path="pinterest/general/create.svg" alt="Create" />
-        </a>
-        <a href="/" className="menuIcon">
+        </Link>
+        <Link to="/" className="menuIcon">
           <Image path="pinterest/general/updates.svg" alt="Updates" />
-        </a>
-        <a href="/" className="menuIcon">
+        </Link>
+        <Link to="/" className="menuIcon">
           <Image path="pinterest/general/messages.svg" alt="Messages" />
-        </a>
+        </Link>
       </div>
-      <a href="/" className="menuIcon">
+      <Link to="/" className="menuIcon">
         <Image path="pinterest/general/settings.svg" alt="Settings" />
-      </a>
+      </Link>
     </div>
   );
 };
--- src/routes/create/create.jsx
+++ src/routes/create/create.jsx
@@ -1,7 +1,61 @@
+import Image from "../../components/image/image";
 import "./create.css";
 
 const Create = () => {
-  return <div className="create">Create</div>;
+  return (
+    <div className="create">
+      <div className="createTop">
+        <h1 className="createTitle">Create Pin</h1>
+        <button className="createButton">Publish</button>
+      </div>
+      <div className="createBottom">
+        <div className="upload">
+          <div className="uploadTitle">
+            <Image path="pinterest/general/upload.svg" alt="upload" />
+            <span>Choose a file</span>
+          </div>
+          <div className="uploadInfo">
+            <span>
+              We recommend using high quality .jpg files less than 20 MB or .mp4
+              files less than 200 MB.
+            </span>
+          </div>
+        </div>
+        <form action="" className="createForm">
+          <div className="createFormItem">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" placeholder="Add a title" />
+          </div>
+          <div className="createFormItem">
+            <label htmlFor="description">Description</label>
+            <textarea
+              rows={6}
+              id="description"
+              placeholder="Add a detailed description"
+            />
+          </div>
+          <div className="createFormItem">
+            <label htmlFor="link">Link</label>
+            <input type="text" id="link" placeholder="Add a link" />
+          </div>
+          <div className="createFormItem">
+            <label htmlFor="board">Board</label>
+            <select name="board" id="board">
+              <option value="">Choose a board</option>
+              <option value="1">Board 1</option>
+              <option value="2">Board 2</option>
+              <option value="3">Board 3</option>
+            </select>
+          </div>
+          <div className="createFormItem">
+            <label htmlFor="tags">Tags</label>
+            <input type="text" id="tags" placeholder="Add tags" />
+            <small>Dont&apos;t worry, people won&apos;t see your tags</small>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Create;
